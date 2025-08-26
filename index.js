cabe9e77db156bbae1ac45152c9ee1ccba86b628
A-Sal3 <asalinas3205@gmail.com>
2025-08-26 10:08:06 -0500

    Infrastructure for highlighting the card on board

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const faces = ["A", "K", "Q", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
 let newId = 0;
 let count = 0;
 let selectedCard = null;
+const highlighted = [] // [[card.id, location1{}, location2{}], ...]
 
 
 const cardBox = document.getElementById('card-text');
@@ -17,6 +18,13 @@ cardBox.addEventListener('keypress', (e) => {
 
 const remove = () => {
   if (selectedCard) {
+    for (const elt of highlighted) {
+      if (elt[0] == selectedCard.id) {
+        elt[1].remove();
+        elt[2].remove();
+        break;
+      }
+    }
     selectedCard.className="deleteCard card"
     setTimeout(() => {
       selectedCard.remove();
@@ -96,11 +104,20 @@ function addCard(face, suit) {
   });
 
   document.getElementById('hand').append(newCard);
+
+  updateBoard(newCard, face, suit);
 }
 
 function cardClicked(newCard, face, suit) {
   if (selectedCard) {
     selectedCard.className = "notSelected card";
+    for (const elt of highlighted) {
+      if (elt[0] == selectedCard.id) {
+        elt[1].className = "highlighter black";
+        elt[2].className = "highlighter black";;
+        break;
+      }
+    }
   }
   selectedCard = newCard;
   changeCard(face, suit);
@@ -114,4 +131,32 @@ function changeCard(face, suit) {
   suit == 'D' ? '♦' :
   suit == 'H' ? '♥' : '♠';
   removeCard.className = (suit == 'D' || suit == 'H') ? "red" : "";
+
+  for (const elt of highlighted) {
+    if (elt[0] == selectedCard.id) {
+      elt[1].className = "highlighter green";
+      elt[2].className = "highlighter green";;
+      break;
+    }
+  }
+}
+
+const x = ["4.6", "13.61", "22.75", "31.88", "41.01", "50.13", "59.3", "68.43", "77.57", "86.7"];
+const y = ["1", "10.85", "20.74", "30.52", "40.25", "50", "59.8", "69.95", "79.9", "89.65"];
+function updateBoard(newCard, face, suit) {
+  const board = document.getElementById('board-container');
+  const loc1 = document.createElement('div');
+  const loc2 = document.createElement('div');
+  loc1.className = "highlighter black";
+  loc2.className = "highlighter black";
+  loc1.addEventListener("click", (e) => {
+    cardClicked(newCard, face, suit);
+  });
+  loc2.addEventListener("click", (e) => {
+    cardClicked(newCard, face, suit);
+  });
+  
+  highlighted.push([newId, loc1, loc2]);
+  board.append(loc1);
+  board.append(loc2);
 }
